c3b278f91d45ee2fde33d7a32519c9534f2f97d2
Eric Yang <eric.shaohuiyang@gmail.com>
2015-11-10 09:30:47 -0800

    structured MVC

--- Checklists/ChecklistViewController.swift
+++ Checklists/ChecklistViewController.swift
@@ -9,7 +9,17 @@
 import UIKit
 
 class ChecklistViewController: UITableViewController {
-
+    var row0text = "Walk the dog"
+    var row1text = "Brush teeth"
+    var row2text = "Learn iOS development"
+    var row3text = "Soccer practice"
+    var row4text = "Eat ice cream"
+    var row0checked = false
+    var row1checked = false
+    var row2checked = false
+    var row3checked = false
+    var row4checked = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -21,7 +31,7 @@ class ChecklistViewController: UITableViewController {
     }
     
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 100
+        return 5
     }
     
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
@@ -29,26 +39,46 @@ class ChecklistViewController: UITableViewController {
         
         let label = cell.viewWithTag(1000) as! UILabel
         
-        if indexPath.row % 5 == 0 { label.text = "Walk the dog"
-        } else if indexPath.row % 5 == 1 {
-            label.text = "Brush my teeth"
-        } else if indexPath.row % 5 == 2 {
-            label.text = "Learn iOS development"
-        } else if indexPath.row % 5 == 3 {
-            label.text = "Soccer practice"
-        } else if indexPath.row % 5 == 4 {
-            label.text = "Eat ice cream"
+        if indexPath.row == 0 { label.text = row0text
+        } else if indexPath.row == 1 {
+            label.text = row1text
+        } else if indexPath.row == 2 {
+            label.text = row2text
+        } else if indexPath.row == 3 {
+            label.text = row3text
+        } else if indexPath.row == 4 {
+            label.text = row4text
         }
+        configureCheckmarkForCell(cell, indexPath: indexPath)
+            
         return cell
+    }
+    
+    func configureCheckmarkForCell(cell: UITableViewCell, indexPath: NSIndexPath) {
+            var isChecked = false
+            if indexPath.row == 0 { isChecked = row0checked
+        } else if indexPath.row == 1 { isChecked = row1checked
+        } else if indexPath.row == 2 { isChecked = row2checked
+        } else if indexPath.row == 3 { isChecked = row3checked
+        } else if indexPath.row == 4 { isChecked = row4checked
+            }
+        if isChecked {
+            cell.accessoryType = .Checkmark
+        } else {
+            cell.accessoryType = .None
+            }
     }
     
     override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
         if let cell = tableView.cellForRowAtIndexPath(indexPath) {
-            if cell.accessoryType == .None {
-                cell.accessoryType = .Checkmark
-            } else {
-                cell.accessoryType = .None
+            if indexPath.row == 0 {
+                row0checked = !row0checked
+            } else if indexPath.row == 1 { row1checked = !row1checked
+            } else if indexPath.row == 2 { row2checked = !row2checked
+            } else if indexPath.row == 3 { row3checked = !row3checked
+            } else if indexPath.row == 4 { row4checked = !row4checked
             }
+            configureCheckmarkForCell(cell, indexPath: indexPath)
         }
         tableView.deselectRowAtIndexPath(indexPath, animated: true)
     }

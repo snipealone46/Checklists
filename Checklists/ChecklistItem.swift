10f322f21c7eef55757dc0029b2ffe5f84461bea
Eric Yang <eric.shaohuiyang@gmail.com>
2015-11-10 17:55:34 -0800

    make listItems objects in a list

---
+++ Checklists/ChecklistItem.swift
@@ -0,0 +1,14 @@
+//
+//  ChecklistItem.swift
+//  Checklists
+//
+//  Created by Shaohui Yang on 11/10/15.
+//  Copyright © 2015 Shaohui. All rights reserved.
+//
+
+import Foundation
+
+class ChecklistItem {
+    var text = ""
+    var checked = false
+}

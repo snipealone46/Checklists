97ad03fa48d3acbe14cac7ffca68c0d9c2e9cd35
Eric Yang <eric.shaohuiyang@gmail.com>
2015-11-14 09:31:13 -0800

    Now has a tableview with cells for Checklists objects
    *the rest of the screen doesn't do much yet

---
+++ Checklists/AllListsViewController.swift
@@ -0,0 +1,63 @@
+//
+//  AllListsViewController.swift
+//  Checklists
+//
+//  Created by Shaohui Yang on 11/13/15.
+//  Copyright © 2015 Shaohui. All rights reserved.
+//
+
+import UIKit
+
+class AllListsViewController: UITableViewController {
+    var lists: [Checklist]
+    
+    required init?(coder aDecoder: NSCoder) {
+        lists = [Checklist]()
+        super.init(coder: aDecoder)
+        
+        var list = Checklist(name: "Birthdays")
+        lists.append(list)
+        list = Checklist(name: "Groceries")
+        lists.append(list)
+        list = Checklist(name: "Cool Apps")
+        lists.append(list)
+        list = Checklist(name: "To Do")
+        lists.append(list)
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return lists.count
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = cellForTableView(tableView)
+        let checklist = lists[indexPath.row]
+        cell.textLabel!.text = checklist.name
+        cell.accessoryType = .DetailDisclosureButton
+        return cell
+        
+    }
+    
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        performSegueWithIdentifier("ShowChecklist", sender: nil)
+    }
+
+    func cellForTableView(tableView: UITableView) -> UITableViewCell{
+        let cellIdentifier = "Cell"
+        if let cell = tableView.dequeueReusableCellWithIdentifier(cellIdentifier) {
+            return cell
+        } else {
+            return UITableViewCell(style: .Default, reuseIdentifier: cellIdentifier)
+        }
+    }
+
+}
---
+++ Checklists/Checklist.swift
@@ -0,0 +1,18 @@
+//
+//  Checklist.swift
+//  Checklists
+//
+//  Created by Shaohui Yang on 11/14/15.
+//  Copyright © 2015 Shaohui. All rights reserved.
+//
+
+import UIKit
+
+class Checklist: NSObject {
+    var name = ""
+    
+    init(name: String) {
+        self.name = name
+        super.init()
+    }
+}
